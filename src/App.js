691aec3007bf5c62665d0b38becd0fafa985d6a2
Dayana Huaytalla <dhuaytalla@imca.edu.pe>
2021-11-07 16:57:05 -0500

    add alerts. Close #8

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Alerts from './components/Alerts/Alerts';
 import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
       <Login />
+      <Alerts/>
     </div>
   );
 }
