f381445a0e38b25322fd20bde581bbdb2c8ac302
Andrea Blanco <estefania_8_3@hotmail.com>
2021-11-06 23:59:13 -0500

    feat: add search view with mui

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,10 +1,26 @@
-
+import { Grid, Box, TextField, InputAdornment } from '@mui/material';
+// import { SearchSharpIcon } from '@mui/icons-material';
+import SearchSharpIcon from '@mui/icons-material/SearchSharp';
 
 const Search = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Grid container>
+        <Grid item xs={12}>
+          <Box
+            sx={{ display: 'flex', alignItems: 'flex-end' }}>
+            <SearchSharpIcon
+              sx={{ color: 'action.active', mr: 1, my: 3 }} />
+            <TextField
+              label="Escribe aquí..."
+              variant="standard"
+              fullWidth
+              helperText="Por favor ingrese lo que desee buscar"
+            />
+          </Box>
+        </Grid>
+      </Grid>
+    </>
   )
 }
 
