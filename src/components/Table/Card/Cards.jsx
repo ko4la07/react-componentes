a129b79b6927fd58822bdeb2da22a678ee03baf1
makemile <mkengya@gmail.com>
2021-11-07 20:29:42 -0500

    cards components

---
+++ src/components/Table/Card/Cards.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Card, CardContent, CardMedia, Button, Typography} from "@mui/material";
+import './main.css'
+
+
+
+export const Cards = () => {
+  
+  return (
+<section className= "card">
+      <Card className= "cardLayout">
+        <CardMedia clasName="cardImg" component="img"
+          height="130"
+          width="150"
+          image="/static/images/cards/contemplative-reptile.jpg"
+          alt="products" className = "cardImg"/>
+        <CardContent className = "cardTitleContent">
+          <Typography className = "cardTitle" componente = "p" variant = "h6">product</Typography>
+        </CardContent>
+      
+          <Button   className = "cardButton" variant="contained"> click</Button>
+        
+      </Card>
+    </section>
+  );
+};
