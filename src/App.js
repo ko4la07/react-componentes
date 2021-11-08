8099fcd36b7c8f50a4853c2d5455c983a16738ab
Dayana Huaytalla <dhuaytalla@imca.edu.pe>
2021-11-07 22:02:48 -0500

    :D

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Tabs from '../src/components/TABS/Tabs'
 
 import './App.css';
 import Alerts from './components/Alerts/Alerts';
-import Login from './components/Login';
+import Login from './components/Login/Login';
 import Box from '@mui/material/Box';
 
 
@@ -17,8 +17,6 @@ function App() {
       <Box sx={{ width: '100%',display: 'flex',mx: 10 ,my:10 }}>
       <Tabs />
       </Box>
-
-      
 
     </div>
   );
